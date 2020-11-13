2043894eda404720ff05d292e85b20228079e547
Roman <roman@onoffapp.com>
2020-11-13 17:46:27 +0200

    'logging event' feature

--- app/src/main/java/com/example/repeatingalarmfoss/helper/FlightRecorder.kt
+++ app/src/main/java/com/example/repeatingalarmfoss/helper/FlightRecorder.kt
@@ -4,8 +4,10 @@ package com.example.repeatingalarmfoss.helper
 
 import android.util.Log
 import com.example.repeatingalarmfoss.BuildConfig
+import com.example.repeatingalarmfoss.helper.extensions.DATE_PATTERN_FOR_LOGGING
 import java.io.File
-import javax.inject.Inject
+import java.text.SimpleDateFormat
+import java.util.*
 import javax.inject.Singleton
 
 /*TODO: current thread mark, encryption*/
@@ -14,11 +16,29 @@ class FlightRecorder constructor(private val logStorage: File) {
     private val isDebug = BuildConfig.DEBUG
     var TAPE_VOLUME = 10 * 1024 * 1024 /** 10 MB **/
 
-    fun i(toPrintInLogcat: Boolean = false, what: () -> String) = clearBeginningIfNeeded("} I {", what).also { logStorage.appendText("} I { ${what.invoke()}\n") }.also { if(toPrintInLogcat && isDebug) { Log.i(this::class.java.simpleName, what.invoke())} }
-    fun d(toPrintInLogcat: Boolean = false, what: () -> String) = clearBeginningIfNeeded("} D {", what).also { logStorage.appendText("} D { ${what.invoke()}\n") }.also { if(toPrintInLogcat && isDebug) { Log.i(this::class.java.simpleName, what.invoke())} }
-    fun w(toPrintInLogcat: Boolean = false, what: () -> String) = clearBeginningIfNeeded("} W {", what).also { logStorage.appendText("} W { ${what.invoke()}\n") }.also { if(toPrintInLogcat && isDebug) { Log.i(this::class.java.simpleName, what.invoke())} }
-    fun e(toPrintInLogcat: Boolean = false, what: () -> String) = clearBeginningIfNeeded("} E {", what).also { logStorage.appendText("} E { ${what.invoke()}\n") }.also { if(toPrintInLogcat && isDebug) { Log.i(this::class.java.simpleName, what.invoke())} }
-    fun wtf(toPrintInLogcat: Boolean = false, what: () -> String) = clearBeginningIfNeeded("} X {", what).also { logStorage.appendText("} X { ${what.invoke()}\n") }.also { if(toPrintInLogcat && isDebug) { Log.i(this::class.java.simpleName, what.invoke())} }
+    /**
+     * @param when - time in milliseconds
+     * */
+    fun logScheduledEvent(toPrintInLogcat: Boolean = false, what: () -> String, `when`: Long) {
+        val message = { what.invoke() + " " + SimpleDateFormat(DATE_PATTERN_FOR_LOGGING, Locale.UK).format(`when`) }
+        clearBeginningIfNeeded("} I {", message)
+            .also { logStorage.appendText("} I { ${message.invoke()}\n") }.also { if(toPrintInLogcat && isDebug) { Log.i(this::class.java.simpleName, message.invoke())} }
+    }
+
+    fun i(toPrintInLogcat: Boolean = false, what: () -> String) = clearBeginningIfNeeded("} I {", what)
+        .also { logStorage.appendText("} I { ${what.invoke()}\n") }.also { if(toPrintInLogcat && isDebug) { Log.i(this::class.java.simpleName, what.invoke())} }
+
+    fun d(toPrintInLogcat: Boolean = false, what: () -> String) = clearBeginningIfNeeded("} D {", what)
+        .also { logStorage.appendText("} D { ${what.invoke()}\n") }.also { if(toPrintInLogcat && isDebug) { Log.i(this::class.java.simpleName, what.invoke())} }
+
+    fun w(toPrintInLogcat: Boolean = false, what: () -> String) = clearBeginningIfNeeded("} W {", what)
+        .also { logStorage.appendText("} W { ${what.invoke()}\n") }.also { if(toPrintInLogcat && isDebug) { Log.i(this::class.java.simpleName, what.invoke())} }
+
+    fun e(toPrintInLogcat: Boolean = false, what: () -> String) = clearBeginningIfNeeded("} E {", what)
+        .also { logStorage.appendText("} E { ${what.invoke()}\n") }.also { if(toPrintInLogcat && isDebug) { Log.i(this::class.java.simpleName, what.invoke())} }
+
+    fun wtf(toPrintInLogcat: Boolean = false, what: () -> String) = clearBeginningIfNeeded("} X {", what)
+        .also { logStorage.appendText("} X { ${what.invoke()}\n") }.also { if(toPrintInLogcat && isDebug) { Log.i(this::class.java.simpleName, what.invoke())} }
 
     private fun clearBeginningIfNeeded(meta: String, what: () -> String) {
         val newDataSize = "$meta ${what.invoke()}\n".toByteArray().size
