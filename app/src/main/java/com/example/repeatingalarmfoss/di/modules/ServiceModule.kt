cd60a7a003ea3dc0d8deb4f846f1b3760cc29ca1
liinahamari <l1bills@protonmail.com>
2020-12-26 19:18:35 +0200

    Declaring services in ServiceModule

--- app/src/main/java/com/example/repeatingalarmfoss/di/modules/ServiceModule.kt
+++ app/src/main/java/com/example/repeatingalarmfoss/di/modules/ServiceModule.kt
@@ -2,6 +2,7 @@ package com.example.repeatingalarmfoss.di.modules
 
 import android.annotation.SuppressLint
 import android.app.AlarmManager
+import android.app.KeyguardManager
 import android.app.NotificationManager
 import android.content.ContentResolver
 import android.content.Context
@@ -11,7 +12,9 @@ import android.hardware.SensorManager
 import android.hardware.camera2.CameraManager
 import android.media.AudioManager
 import android.os.BatteryManager
+import android.os.PowerManager
 import android.os.Vibrator
+import android.view.WindowManager
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -34,11 +37,19 @@ class ServiceModule {
 
     @Provides
     @Singleton
+    fun provideWakeLock(context: Context): PowerManager.WakeLock = (context.getSystemService(POWER_SERVICE) as PowerManager).newWakeLock(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON, javaClass.simpleName)
+
+    @Provides
+    @Singleton
     fun provideSensorManager(context: Context): SensorManager = context.getSystemService(SENSOR_SERVICE) as SensorManager
 
     @Provides
     @Singleton
     fun provideAudioManager(context: Context): AudioManager? = context.getSystemService(AUDIO_SERVICE) as AudioManager?
+
+    @Provides
+    @Singleton
+    fun provideKeyguardManager(context: Context): KeyguardManager = context.getSystemService(KEYGUARD_SERVICE) as KeyguardManager
 
     @Provides
     @Singleton
